e972af542d44520a6ae7354f4e4f40a0efa4a609
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-28 19:05:48 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,7 +1,10 @@
 class CreateStudent < ActiveMigration[5.2]
-  def change
-   create_table :students do |t|
-     t.string :first_name
-     t.string :last_name 
-     
-     
+    def change
+     create_table :students do |t|
+       t.string :first_name
+       t.string :last_name 
+       
+       t.timestamps null:false
+     end 
+   end 
+ end 
